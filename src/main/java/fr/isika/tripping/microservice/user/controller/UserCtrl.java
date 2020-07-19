1fd2d1b30b94291100e9ecd8ba5bbf451ee99b9d
SofiaVP <sofiavayas@hotmail.com>
2020-07-19 16:35:44 +0200

    CROSS added to ctrl

--- src/main/java/fr/isika/tripping/microservice/user/controller/UserCtrl.java
+++ src/main/java/fr/isika/tripping/microservice/user/controller/UserCtrl.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import fr.isika.tripping.microservice.user.model.UserEntity;
 import fr.isika.tripping.microservice.user.repository.UserRepo;
 
+@CrossOrigin("*")
 @Controller
 @RestController
 @RequestMapping (path = "/tripping/user")
